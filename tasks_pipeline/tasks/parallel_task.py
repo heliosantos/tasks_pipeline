f2a09d2cc2659fe10ec57c34c2365489f90dfef1
Hélio Santos <heliosantos99@gmail.com>
2023-09-17 01:47:45 +0100

    limit concurrency on parallel tasks

--- tasks_pipeline/tasks/parallel_task.py
+++ tasks_pipeline/tasks/parallel_task.py
@@ -5,13 +5,25 @@ from .task_status import TaskStatus
 
 class ParallelTask(BaseTask):
 
-    def __init__(self, name, tasks=[]):
+    def __init__(self, name, maxConcurrency=None, tasks=[]):
         super().__init__(name)
+        self.maxConcurrency = maxConcurrency
         self.tasks = tasks
 
     async def run(self):
         await super().run()
-        await asyncio.gather(*[task.run() for task in self.tasks])
+
+        if self.maxConcurrency:
+            semaphore = asyncio.Semaphore(self.maxConcurrency)
+
+            async def f(coro):
+                async with semaphore:
+                    return await coro
+        else:
+            async def f(coro):
+                return await coro
+
+        await asyncio.gather(*[f(task.run()) for task in self.tasks])
 
         if any((task for task in self.tasks if task.status != TaskStatus.COMPLETED)):
             await super().complete(TaskStatus.ERROR)
