b42ff0db06a0506bb00590b26dcc7184e4ff192b
Hélio Santos <heliosantos99@gmail.com>
2023-09-30 22:06:40 +0100

    move create TaskModel logic to PipelineModel

--- tasks_pipeline/pipeline.py
+++ tasks_pipeline/pipeline.py
@@ -1,50 +1,12 @@
 import asyncio
 import curses
-import importlib
 import sys
 
 from .tasks_logger import setup_loggers
 from .view import display
 from .controller import process_input
 from .pipeline_model import PipelineModel
-from .task_model import TaskModel
 from .config import load_config
-
-
-taskIndex = 0
-
-defaultNames = {
-    "SequentialTask": "⭣",
-    "ParallelTask": "⮆",
-    "RetryTask": "↻",
-}
-
-
-def create_task_model(task, parentTaskModel=None):
-    global taskIndex
-    taskIndex += 1
-    t = task["type"]
-    t = t.split(".")
-    if len(t) == 1:
-        t = ["tasks_pipeline"] + t
-    mod, cls = t
-    mod = importlib.import_module(mod)
-    cls = getattr(mod, cls)
-
-    taskName = task.get("name", "")
-    if defaultName := defaultNames.get(task["type"], ""):
-        taskName = f"{defaultName} {taskName}"
-
-    taskModel = TaskModel(taskName, cls(taskName, **task.get("params", {})), taskIndex=taskIndex)
-
-    if parentTaskModel:
-        parentTaskModel.subtasks.append(taskModel)
-        parentTaskModel.task.tasks.append(taskModel.task)
-
-    for child in task.get("tasks", []):
-        create_task_model(child, taskModel)
-
-    return taskModel
 
 
 async def main(stdscr):
@@ -60,15 +22,12 @@ async def main(stdscr):
 
     setup_loggers(config.get("logging"))
 
-    rootTask = config["rootTask"]
     title = config.setdefault("title", "Tasks Pipeline")
 
-    rootTaskModel = create_task_model(rootTask)
+    pipelineModel = PipelineModel(config)
 
-    model = PipelineModel(rootTaskModel)
-
-    asyncio.create_task(display(stdscr, model, title))
-    await process_input(stdscr, model)
+    asyncio.create_task(display(stdscr, pipelineModel, title))
+    await process_input(stdscr, pipelineModel)
 
     curses.nocbreak()
     stdscr.keypad(False)
--- tasks_pipeline/pipeline_model.py
+++ tasks_pipeline/pipeline_model.py
@@ -1,6 +1,8 @@
+import importlib
 from enum import Enum, auto
 
 from .util import flatten_tasks
+from .task_model import TaskModel
 
 
 class InputMode(Enum):
@@ -10,14 +12,58 @@ class InputMode(Enum):
 
 
 class PipelineModel:
-    def __init__(self, rootTask):
-        self.rootTask = rootTask
-        self.tasks = flatten_tasks(rootTask)
+    def __init__(self, config):
+        self.load_config(config)
+
+    def selectTask(self, key):
+        matching = list(filter(lambda t: t.taskIndex == int(key), self.tasks))
+        self.selectedTask = matching[0] if matching else None
+
+    def load_config(self, config=None):
+        if config:
+            self.config = config
+
+        self.rootTask = create_task_models(self.config["rootTask"])
+        self.tasks = flatten_tasks(self.rootTask)
         self.inputMode: InputMode = InputMode.NONE
         self.hasUpdates: bool = True
         self.selectedTask = None
         self.selectedTaskText = ""
 
-    def selectTask(self, key):
-        matching = list(filter(lambda t: t.taskIndex == int(key), self.tasks))
-        self.selectedTask = matching[0] if matching else None
+
+def create_task_models(rootTask):
+    taskIndex = 0
+
+    defaultNames = {
+        "SequentialTask": "⭣",
+        "ParallelTask": "⮆",
+        "RetryTask": "↻",
+    }
+
+    def create_task_model(task, parentTaskModel=None):
+        nonlocal taskIndex
+        taskIndex += 1
+        t = task["type"]
+        t = t.split(".")
+        if len(t) == 1:
+            t = ["tasks_pipeline"] + t
+        mod, cls = t
+        mod = importlib.import_module(mod)
+        cls = getattr(mod, cls)
+
+        taskName = task.get("name", "")
+        if defaultName := defaultNames.get(task["type"], ""):
+            taskName = f"{defaultName} {taskName}"
+
+        taskModel = TaskModel(taskName, cls(taskName, **task.get("params", {})), taskIndex=taskIndex)
+
+        if parentTaskModel:
+            parentTaskModel.subtasks.append(taskModel)
+            parentTaskModel.task.tasks.append(taskModel.task)
+
+        for child in task.get("tasks", []):
+            create_task_model(child, taskModel)
+
+        return taskModel
+
+    return create_task_model(rootTask)
